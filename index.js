bde05726df468ebdbcfbbe5a3dcdf7c8d6c9d670
Rupesh457 <rupeshmahajan457@gmail.com>
2022-08-05 23:44:11 +0530

    1st

---
+++ index.js
@@ -0,0 +1,10 @@
+function loadcoupon(){
+    document.getElementById('coupon').style.display ="block"
+}
+function closecoupon(){
+    document.getElementById('coupon').style.display = "none"
+}
+
+
+
+     
